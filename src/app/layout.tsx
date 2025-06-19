6421eed6f13f3d48b3386b92a544d9063a869779
Aaron Kane <Aaron.John.Kane@gmail.com>
2025-06-19 01:07:15 +0000

    Ok.  Where do I put my google analytics number?  G-83X1S10317?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script'; // Import the Script component
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/contexts/AuthContext';
@@ -20,14 +21,25 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
-        {/* Existing Inter font link - remove if not explicitly needed, as we defined new body/headline fonts */}
-        {/* <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" /> */}
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
         <AuthProvider>
           {children}
         </AuthProvider>
         <Toaster />
+        {/* Google Analytics Scripts */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-83X1S10317"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-83X1S10317');
+          `}
+        </Script>
       </body>
     </html>
   );
